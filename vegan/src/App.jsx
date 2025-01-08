1fcebdd2c8db2250c034c1af9256c02348f08bf4
pedrodamiao26061998 <pedrodamiao26061998@gmail.com>
2025-01-08 13:32:19 +0000

    terminei o footer

--- vegan/src/App.jsx
+++ vegan/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './components/Sidebar/sidebar'
 import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
      <Sidebar />
      <Outlet />
+     <Footer />
     </>
   )
 }
